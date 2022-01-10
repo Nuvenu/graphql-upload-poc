e2517335b9a939613c1fbf1f2e57379253c9a20b
Briet Sparks <brietsparks@gmail.com>
2022-01-09 21:00:45 -0600

    createPost

--- frontend-graphql-file-upload/src/App.js
+++ frontend-graphql-file-upload/src/App.js
@@ -5,19 +5,25 @@ import { gql, GraphQLClient } from 'graphql-request'
 function App() {
   const endpoint = 'http://localhost:4000/graphql'
   const graphQLClient = new GraphQLClient(endpoint)
+  const [establishmentProfileId, setEstablishmentProfileId] = useState();
   const [file, setFile] = useState(null)
   const [caption, setCaption] = useState('')
   const UPLOAD_FILE = gql`
-      mutation SingleUpload($file: Upload!, $caption: String! ) {
-          singleUpload(file: $file, caption: $caption ) {
-              filename
-              mimetype
-              encoding
+      mutation createPost($params: CreatePostParams!) {
+          createPost(params: $params) {
+              id
           }
       }
   `;
+
   const handleFileUpload = async () => {
-    const data = await graphQLClient.request(UPLOAD_FILE, { file: file, caption: caption })
+    const data = await graphQLClient.request(UPLOAD_FILE, {
+      params: {
+        ownerId: establishmentProfileId,
+        primaryText: caption,
+        imageFile: file
+      }
+    })
     console.log(JSON.stringify(data, undefined, 2))
   }
 
@@ -30,23 +36,45 @@ function App() {
   }
   return (
     <div className="App">
-      <form encType={'multipart/form-data'} className={'form'}>
-        <input type="text" onChange={onCaptionChange}
-               style={{ marginBottom: '1rem', height: '2rem', width: '10rem', borderRadius: '.3rem' }}
-               placeholder={'Add caption'}/>
-        <input type="file" onChange={onFileChange}/>
-      </form>
+      <div>
+        <input
+          type="text"
+          onChange={e => setEstablishmentProfileId(e.target.value)}
+          style={{ marginBottom: '1rem', height: '2rem', width: '10rem', borderRadius: '.3rem' }}
+          placeholder="Profile ID"
+        />
+      </div>
 
-      <button className={'btn'} onClick={handleFileUpload}
-              style={{
-                height: '2rem',
-                width: '5rem',
-                backgroundColor: '#086E7D',
-                color: 'white',
-                borderRadius: '.5rem'
-              }}>
-        Upload
-      </button>
+      <div>
+        <input
+          type="text"
+          onChange={onCaptionChange}
+          style={{ marginBottom: '1rem', height: '2rem', width: '10rem', borderRadius: '.3rem' }}
+          placeholder={'Caption'}
+        />
+      </div>
+
+      <div>
+        <input type="file" onChange={onFileChange}/>
+      </div>
+
+      <br/>
+
+      <div>
+        <button
+          className={'btn'}
+          onClick={handleFileUpload}
+          style={{
+            height: '2rem',
+            width: '5rem',
+            backgroundColor: '#086E7D',
+            color: 'white',
+            borderRadius: '.5rem'
+          }}
+        >
+          Upload
+        </button>
+      </div>
     </div>
   );
 }
